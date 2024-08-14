fb4cf7f713e033c3b71bcebd0087e3a7066217f9
ofion87 <beltran_junior@hotmail.com>
2024-08-14 01:48:18 -0300

    Removendo arquivos desnecessários

--- pos_descomplica/teste.py
+++
@@ -1,4 +0,0 @@
-dataNascimento= int(input("entre com a sua data de nascimento: "))
-dataAtual= int(input("entre com o ano atual"))
-idade = dataAtual-dataNascimento
-print("Qual o seu titulo de eleitor") if idade>=18 else print("informe o RG do responsavel")
